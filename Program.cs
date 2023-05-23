890162e4899134ede55ee358cdc4c6ce37f4e6bd
ManderO9 <hossem.chetoui@gmail.com>
2023-05-23 09:20:11 +0100

    fix headers

--- Program.cs
+++ Program.cs
@@ -9,7 +9,10 @@ app.MapGet("/", async (context) =>
 {
     // Set status code to 200
     context.Response.StatusCode = 200;
+    // Set content to type to html
 
+    context.Response.ContentType = MediaTypeNames.Text.Html;
+    
     // Get the path of the web root folder
     var path = context.RequestServices.GetRequiredService<IWebHostEnvironment>().WebRootPath;
     
@@ -18,8 +21,7 @@ app.MapGet("/", async (context) =>
                // Write the content of the file to the response body          
                .CopyToAsync(context.Response.Body);
 
-    // Set content to type to html
-    context.Response.ContentType = MediaTypeNames.Text.Html;;
+
 });
 
 app.Run();
